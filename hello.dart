0c0e4ea5b6d5b55b02931c33ddbcceb99c00d01a
MdShakil14 <mdshakil1420414@gmail.com>
2022-01-18 16:43:50 +0600

    Starting dart basic

---
+++ hello.dart
@@ -0,0 +1,45 @@
+
+import 'dart:io';
+main(){
+  // Variable in dart
+    // var firstName = 'Mohammad';
+    // String lastName = 'Shakil';
+    // print(firstName + ' ' + lastName);
+    //  bool isitTrue = true;
+    //  dynamic h1 = 'Shakil';
+    //  h1 = 1;
+    //  print ('$h1');
+    //  const name = 4;
+
+
+  // take input from user
+    //  stdout.writeln('What is your name?');
+    //  var name = stdin.readLineSync();
+    //  print('my name is $name');
+
+  // multiline string
+    //  String multiline_string = ''' my name is Shakil and
+    //  i am from Tangail''';
+    //  print(multiline_string)  ; 
+
+  //Data type conversion
+    //String to integer
+    // var one = int.parse('34');
+    // assert(one == 34);  
+    // print(one);
+    //String to double
+    // var onePoint = double.parse('34.5');
+    // assert(onePoint == 34.5);
+    //Integer to string
+    // String oneString = 1.toString();
+    // assert(oneString == '1');
+    //Double to string
+    // String onePointString = 3.14159.toStringAsFixed(2);
+    // print(oneString + '  ' + onePointString);
+
+    // Null aware operator
+
+    int? number;
+    print(number);
+
+}
